50afb98537f08f1b401194a317ac47fb5adae14c
Marco Muñoz <marco@MacBook-Pro-de-Marco-2.local>
2023-05-15 23:18:50 +0200

    creando funcion nuevo registro

--- registro_ig/routes.py
+++ registro_ig/routes.py
@@ -1,6 +1,8 @@
 from registro_ig import app, VERSION
-from flask import jsonify, render_template
+from flask import jsonify, render_template,request
 from registro_ig.models import *
+import sqlite3
+from http import HTTPStatus
 
 @app.route("/")
 def index():
@@ -9,11 +11,41 @@ def index():
 @app.route(f"/api/{VERSION}/all")
 def all_movements():
     datos= select_all()
-    return jsonify(datos)
+
+    try:
+        return jsonify(
+         { 
+            "data": datos,
+            "status": "OK"
+         }
+    ),HTTPStatus.OK        
+    except sqlite3.Error as e:
+        return jsonify(
+         { 
+            "data": str(e),
+            "status": "OK"
+         }
+    ),HTTPStatus.BAD_REQUEST
+
+
 
 @app.route(f"/api/{VERSION}/new", methods=["POST"])
 def new():
-    return "Aqui realizamos un alta"
+    registro = request.json
+    try:
+        insert([registro['date'],[registro['concept'],registro['quantity']]])
+        return jsonify(
+         { 
+            "status": "OK"
+         }
+    ),HTTPStatus.CREATED        
+    except sqlite3.Error as e:
+        return jsonify(
+         { 
+            "data": str(e),
+            "status": "OK"
+         }
+    ),HTTPStatus.NOT_FOUND
 
 @app.route(f"/api/{VERSION}/update/<int:id>", methods=["PUT"])
 def update():
--- registro_ig/static/js/app.js
+++ registro_ig/static/js/app.js
@@ -1,15 +1,88 @@
 let peticion_movimientos = new XMLHttpRequest() //creo un objeto de tipo XMLHttpRequest
+let peticion_alta = new XMLHttpRequest() // creo objeto para peticion alta
 
 //funcion que recibe la respuesta del servidor 
 function peticion_movimientos_handler(){
     if(this.readyState == 4 ){
-        if(this.satatus == 200){
+        if(this.status == 200){
             const datajson = JSON.parse(this.responseText);
-            alert(datajson)
+
+            const tabla = document.getElementById("movements_table");
+
+            const movimientos = datajson.data
+
+            for (let i = 0; i < movimientos.length; i++) {
+                console.log(movimientos[i]);
+                const fila = document.createElement("tr");
+
+                const celda_date = document.createElement("td")
+                celda_date.innerHTML = movimientos[i].date;
+                fila.appendChild(celda_date);
+
+                const celda_concept = document.createElement("td")
+                celda_concept.innerHTML = movimientos[i].concept;
+                fila.appendChild(celda_concept);
+
+                const celda_quantity= document.createElement("td")
+                celda_quantity.innerHTML = movimientos[i].quantity;
+                fila.appendChild(celda_quantity);
+
+                tabla.appendChild(fila);
+            }
+  
+            alert(datajson[1].date)
         }else{
             alert("se ha producido un error en la consulta");
         }
     }
+}
+
+function peticion_alta_handler(){
+    if(this.readyState === 4){
+        if(this.status === 200){
+            
+        }
+    }
+}
+
+function altaMovimiento(event){
+    event.preventDefault();
+    //capturar los valores del imput
+
+    const date = document.getElementById("date").value;
+    const concept = document.getElementById("concept").value;
+    const quantity = document.getElementById("quantity").value;
+
+    //capturar la fecha actual en formato yyyy-mm-dd
+    const hoy = new Date().toISOString().split('T')[0];
+
+    if(!date || date > hoy){
+        alert("Debe agregar una fecha menor o igual a hoy");
+        return
+    }
+
+    if(concept === ""){
+        alert("Debe agregar el concepto")
+    }
+    if(quantity === '0' || quantity === ""){
+        alert("Debes agregar un monto positivo o negativo")
+    }
+    //aqui el codigo para registro
+    peticion_alta.open("POST","http://127.0.0.1:5000/api/v1.0/new", true);
+    peticion_alta.onload = peticion_alta_handler
+    peticion_alta.onerror = function(){alert("No se ha podido completar la peticion de registro")}
+    peticion_alta.setRequestHeader("Content-Type","application/json");
+
+    //definir la estructura json a enviar 
+    const data_json = JSON.stringify(
+        {   
+            date: date,
+            concept: concept,
+            quantity: quantity
+        }
+    )
+
+    peticion_alta.send(data_json); 
 
 }
 
@@ -29,6 +102,9 @@ window.onload=function(){
 
     let boton_cerrar = document.getElementById('btn_cerrar');
     boton_cerrar.addEventListener("click",ocultarForm)
+
+    let boton_guardar = document.getElementById("btn_guardar")
+    boton_guardar.addEventListener("click",altaMovimiento)
 
     //definicion de metodo get y ruta para devoluvion de datos
     peticion_movimientos.open("GET","http://127.0.0.1:5000/api/v1.0/all",true);
